e11d7f8aa0dc1e49ea24c9f0809202b61c59e8ed
lubigsis <lucre.baluc@gmail.com>
2023-03-29 20:16:13 -0300

    Se crean las 7 pilas de cartas en la parte inferior.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //referencias
 let mazo = [];
 let barajado = [];
+let pilas = [];
+
+
 
 const botonEmpezarJuego = document.querySelector(".empezar");
 const tipos = ["trebol", "diamante", "corazon", "espada"];
@@ -20,6 +23,7 @@ const crearMazo = () => {
       for (let j = 0; j < tipos.length; j++) {
         const carta = {
           numero: i,
+          tipo: tipos[j],
           color: colores[tipos[j]],
           estaDadaVuelta: true,
           img: `${i}_de_${tipos[j]}`,
@@ -37,11 +41,61 @@ const crearMazo = () => {
       .map(({ value }) => value);
   };
 
+
+/*con pilas.push se arma una matriz: pilas = [
+  [], [], []
+]
+*/
+
+  const servir = () => {
+    //Esta función quita una carta de barajado
+    //La agrega a una de las 7 pilas de cartas inferiores
+    //cada vez que este for me cuente, se hace una lista nueva:
+//pilas =[];
+    for(let i = 0; i < 7; i++){
+      pilas.push([]);
+      for(let j = 0; j < i + 1; j++){
+        const primeraCartaDeBarajado = barajado[0];
+        barajado.shift();
+        pilas[i].push(primeraCartaDeBarajado);
+      }
+    }
+    //Repita esta acción muchas veces.
+  }
+
+
+  const ponerCartasEnLasPilas = () => {
+    //quiero agarrar las cartas que están en las pilas
+    //construir a partir de ellas un elemento de HTML
+    //agregarles la imagen correspondiente
+    //guardarlas en el elemento HTML correspondiente.
+
+    for (let i = 0; i < pilas.length; i++){
+      const pila = document.querySelector(`#pila-${i}`)
+      for(let j = 0; j < pilas[i].length; j++){
+        const carta = pilas[i][j];
+        const cartaHTML = document.createElement('div');
+        const imagen = document.createElement('img');
+        imagen.src = carta.img;
+        cartaHTML.appendChild(imagen);
+        pila.appendChild(cartaHTML);
+      }
+    }
+  }
+
+
   //-----------------------inicio
   
 botonEmpezarJuego.onclick = () => {
     crearMazo();
     barajar();
+    servir();
+    ponerCartasEnLasPilas();
+   // ponerCartasEnLaPilaInicial();
 
   };
+
+
+
+
 
